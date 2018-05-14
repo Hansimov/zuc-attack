bb79c4573bb91a421062a1a5326166ac2d967c95
Hansimov <591172499@qq.com>
2018-05-14 20:58:40 +0800

    + Add data conversion functions

--- parseTrace.py
+++ parseTrace.py
@@ -71,28 +71,136 @@
 
 # ---------------------------------------------------------------------------------------------- %
 
+# import numpy as np
+import struct
 
-trsfilename = 'celcom.trs'
+# trsfilename = 'celcom.trs'
+trsfilename = 'zuc_traces.trs'
+
+
+def readValueOfTag(fid):
+    lem = int(fid.read(1).hex(), 16)
+    # print('lem: {}'.format(lem))
+    val = fid.read(lem).hex()
+    # print('val: {}'.format(val))
+    return val
+
+# def readValue(file_id, byte_num)
+
+def readHeader(fid):
+    trs_info = {}
+    trs_info['nt'] = TrsTag(0, 'Number of Traces')
+    trs_info['ns'] = TrsTag(0, 'Number of Samples')
+    trs_info['sc'] = TrsTag(0, 'Sample Coding')
+    trs_info['st'] = TrsTag('int8', 'Sample Type')
+    trs_info['ss'] = TrsTag(1, 'Sample Size (Bytes)')
+    trs_info['ds'] = TrsTag(0, 'Data Size')
+    trs_info['ts'] = TrsTag(0, 'Title Space')
+    trs_info['gt'] = TrsTag('trace','Global Title')
+    trs_info['dc'] = TrsTag('', 'Description')
+    trs_info['xo'] = TrsTag(0, 'X-axis Offset')
+    trs_info['xl'] = TrsTag('', 'X-axis Label')
+    trs_info['yl'] = TrsTag('', 'Y-axis Label')
+    trs_info['xs'] = TrsTag(1, 'X-axis Scale')
+    trs_info['ys'] = TrsTag(1, 'Y-axis Scale')
+    trs_info['to'] = TrsTag(0, 'Trace Offset')
+    trs_info['ls'] = TrsTag(0, 'Log Scale')
+
+    tag = fid.read(1).hex().upper()
+    # print(tag)
+    # print(type(tag))
+
+    readmode = 1
+    while readmode == 1:
+        # print(tag)
+        val = readValueOfTag(fid)
+
+        if tag == '41':
+            val = hex2int(val)
+            trs_info['nt'] = TrsTag(val, 'Number of Traces')
+        elif tag == '42':
+            val = hex2int(val)
+            trs_info['ns'] = TrsTag(val, 'Number of Samples')
+        elif tag == '43':
+            trs_info['sc'] = TrsTag(val, 'Sample Coding')
+            # trs_info['st'] = TrsTag(val)
+        elif tag == '44':
+            val = hex2dec(val)
+            trs_info['ds'] = TrsTag(val, 'Data Size')
+        elif tag == '45':
+            trs_info['ts'] = TrsTag(val, 'Title Space')
+        elif tag == '46':
+            # Convert from ASCII string encoded in Hex to plain ASCII?
+            #   https://stackoverflow.com/a/27519487/8328786
+            val = hex2ascii(val)
+            trs_info['gt'] = TrsTag(val, 'Global Title')
+        elif tag == '47':
+            val = hex2ascii(val)
+            trs_info['dc'] = TrsTag(val, 'Description')
+        elif tag == '48':
+            val = hex2dec(val)
+            trs_info['xo'] = TrsTag(val, 'X-axis Offset')
+        elif tag == '49':
+            val = hex2ascii(val)
+            trs_info['xl'] = TrsTag(val, 'X-axis Label')
+        elif tag == '4A':
+            val = hex2ascii(val)
+            trs_info['yl'] = TrsTag(val, 'Y-axis Label')
+        elif tag == '4B':
+            val = hex2float(val)
+            trs_info['xs'] = TrsTag(val, 'X-axis Scale')
+        elif tag == '4C':
+            val = hex2float(val)
+            trs_info['ys'] = TrsTag(val, 'Y-axis Scale')
+        elif tag == '4D':
+            val = hex2dec(val)
+            trs_info['to'] = TrsTag(val, 'Trace Offset')
+        elif tag == '4E':
+            trs_info['ls'] = TrsTag(val, 'Log Scale')
+        elif tag == '5F':
+            readmode = 2; # Jump out of while loop: avoid executing tag reading below
+            break;
+        else:
+            print('Invalid tag: {}'.format(tag))
+            break
+
+        tag = fid.read(1).hex().upper()
+
+    return trs_info
 
 class TrsTag:
-    def __init__(self,val,str):
+    def __init__(self, val, str):
         self.val = val
         self.str = str
 
 
 # What's the correct way to convert bytes to a hex string in Python 3?
 #   https://stackoverflow.com/a/36149089/8328786
-# https://docs.python.org/3/library/stdtypes.html#bytes.hex
-trs_tag_dict = {}
+#   https://docs.python.org/3/library/stdtypes.html#bytes.hex
+
+
+# Convert hex to float
+#   https://stackoverflow.com/a/1592362/8328786
+# Python使用struct处理二进制
+#   https://www.cnblogs.com/gala/archive/2011/09/22/2184801.html
+
+def hex2float(hex_str):
+    float_num = struct.unpack('f',bytearray.fromhex(hex_str))[0]
+    return float_num
+def hex2int(hex_str):
+    int_num = struct.unpack('i', bytearray.fromhex(hex_str))[0]
+    return int_num
+def hex2ascii(hex_str):
+    ascii_str = bytes.fromhex(hex_str).decode()
+    return ascii_str
+def hex2dec(hex_str):
+    dec_num = int(hex_str, 16)
+    return dec_num
+
+
 with open(trsfilename,'rb') as trsfile:
-    for i in range(1,10):
-        a = trsfile.read(1)
-        # print(a.hex())
-        tmp = TrsTag(a.hex(),i**2)
-        dd[i] = tmp
-        print(dd[i].val)
+    trs_info = readHeader(trsfile)
 
-
-for key,val in dd.items():
-    print(key,val.str)
+    for key, item in trs_info.items():
+        print(key, item.str, item.val)
 
